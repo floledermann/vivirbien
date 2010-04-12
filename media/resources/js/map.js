9649199a912df4585ceb56f218ee393388f8d7a3
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2010-04-12 21:19:13 +0200

    OpenLayers first steps

--- media/resources/js/map.js
+++ media/resources/js/map.js
@@ -21,15 +21,14 @@ $(document).ready(function(){
         displayProjection: new OpenLayers.Projection("EPSG:4326")
     } );
 
-    layerMapnik = new OpenLayers.Layer.OSM.Mapnik("OpenStreetMap");
-    layerMapnik.attribution = 'Map ' + layerMapnik.attribution;
-    map.addLayer(layerMapnik);
-    layerMarkers = new OpenLayers.Layer.Markers("Resources");
-    layerMarkers.attribution = 'Resource Data CC-By-NC-SA by <a href="http://vivirbien.mediavirus.org/">Vivir Bien</a>';
+    layer_osm = new OpenLayers.Layer.OSM.Mapnik("OpenStreetMap");
+    layer_osm.attribution = 'Map ' + layer_osm.attribution;
+    map.addLayer(layer_osm);
     
     // http://openflights.org/blog/2009/10/21/customized-openlayers-cluster-strategies/
     //var strategy = new OpenLayers.Strategy.Cluster({distance: 15, threshold: 3});
     
+    /*
     var style = new OpenLayers.Style({
         externalGraphic: "${getIcon}",
         graphicWidth:"21",
@@ -45,39 +44,53 @@ $(document).ready(function(){
         }
     }
     );
+    */
     
-    //layerMarkers.strategies = [strategy];
-    //layerMarkers.styleMap = new OpenLayers.StyleMap({"default": style});
-     
-    map.addLayer(layerMarkers);
-
     var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
     map.setCenter (lonLat, zoom);
 
-    var size = new OpenLayers.Size(21,25);
-    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    var icon = new OpenLayers.Icon('http://www.openstreetmap.org/openlayers/img/marker.png',size,offset);    
-    var lonLat2 = new OpenLayers.LonLat(lon + 0.01, lat + 0.01).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+    var style = new OpenLayers.Style({
+        externalGraphic: "/media/images/marker.png",
+        graphicWidth: 21,
+        graphicHeight: 25,
+        graphicXOffset: -10,
+        graphicYOffset: -25,
+        graphicTitle: '${title}'
+    },{
+        context: {
+            title: function(feature) {
+                return (feature.cluster) ? feature.cluster.length + ' resources' : feature.data.title;
+            }
+        }
+    });
 
-    layerMarkers.addMarker(new OpenLayers.Marker(lonLat, icon));
-    layerMarkers.addMarker(new OpenLayers.Marker(lonLat2, icon.clone()));
-
-
-    /*
-    var style_mark = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
-    style_mark.graphicWidth = 24;
-    style_mark.graphicHeight = 20;
-    style_mark.graphicXOffset = -(style_mark.graphicWidth/2);  // this is the default value
-    style_mark.graphicYOffset = -style_mark.graphicHeight;
-    style_mark.externalGraphic = "marker.png";
-    style_mark.graphicTitle = "this is a test tooltip";
-   
-    var point = new OpenLayers.Geometry.Point(lon, lat);
+    var strategy = new OpenLayers.Strategy.Cluster({distance: 8, threshold: 2});
     
-    var pointFeature = new OpenLayers.Feature.Vector(point, null, style_mark);
-    layerMarkers.addFeatures([pointFeature]);
-    */
+    layer_vector = new OpenLayers.Layer.Vector("Vectors", {
+        attribution: 'Resource Data CC-By-NC-SA by <a href="http://vivirbien.mediavirus.org/">Vivir Bien</a>',
+        styleMap: new OpenLayers.StyleMap({"default": style}),
+        strategies: [strategy]
+    });
     
+    map.addLayer(layer_vector);
+    
+    
+    var proj = new OpenLayers.Projection("EPSG:4326");
+    
+    var point = new OpenLayers.Geometry.Point(lon,lat).transform(proj, map.getProjectionObject());
+    var pointFeature = new OpenLayers.Feature.Vector(point, {title: 'Marker 1'});
+    
+    var point2 = new OpenLayers.Geometry.Point(lon+0.01,lat+0.01).transform(proj, map.getProjectionObject());
+    var pointFeature2 = new OpenLayers.Feature.Vector(point2, {title: 'Marker 2'});
+    
+    layer_vector.addFeatures([pointFeature,pointFeature2]);
+        
+    //var control = new OpenLayers.Control.DrawFeature(layer_vector, OpenLayers.Handler.Point);
+    //map.addControl(control);
+    //control.activate();
+    
+    
+
 });
 
 
