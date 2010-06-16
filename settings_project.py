f9c0622268fd777d028c5e2f970184fa66167876
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2010-06-16 08:17:11 +0200

    added project settings

---
+++ settings_project.py
@@ -0,0 +1,13 @@
+
+PROJECT_NAME = 'vivirbien'
+
+PROJECT_DOMAIN = 'vivirbien'
+
+TRANSLATE_APPS = [
+    #'env/src/django-visual-portfolio/portfolio'
+]
+
+APP_MEDIA = [
+    #('admin','env/src/django-grappelli/grappelli/media/'),
+    ('resources','current-release/src/resources/media/resources/'),
+]
